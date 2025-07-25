7ac0022892d4be9b9a1db3dcc07f2157cd452279
Aditi Kesarwani <aditi_k08@outlook.com>
2025-07-25 11:29:35 -0700

    delete part added

--- BirthdaysApp/ContentView.swift
+++ BirthdaysApp/ContentView.swift
@@ -15,12 +15,18 @@ struct ContentView: View {
     @State private var newBirthday = Date.now
     var body: some View {
         NavigationStack{
-        List(friends) { friend in
-            HStack{
-                Text(friend.name)
-                Spacer()
-                Text(friend.birthday, format: .dateTime.month(.wide).day().year())
-            }
+            List{
+                ForEach(friends) { friend in
+                    HStack{
+                        
+                        HStack{
+                            Text(friend.name)
+                            Spacer()
+                            Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                        }
+                    }
+                }
+                .onDelete(perform: deleteFriend)
             }
         .navigationTitle("Birthdays")
         .safeAreaInset(edge: .bottom){

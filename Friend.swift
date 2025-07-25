5bd59799acb423b286db564500cdece53e32ffd4
Aditi Kesarwani <aditi_k08@outlook.com>
2025-07-25 10:44:13 -0700

    added first half

---
+++ Friend.swift
@@ -0,0 +1,19 @@
+//
+//  Friend.swift
+//  BirthdaysApp
+//
+//  Created by Scholar on 7/25/25.
+//
+
+
+import Foundation
+
+class Friend{
+    var name: String
+    var birthday: Date
+    
+    init(fName: String, fbday: Date){
+        name = fName
+        birthday = fbday
+    }
+}
